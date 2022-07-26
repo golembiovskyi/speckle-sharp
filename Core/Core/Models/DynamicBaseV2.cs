57cad95588e6c67391923682f58bfc0bc044b64a
Dimitrie Stefanescu <didimitrie@gmail.com>
2022-07-26 09:00:50 +0300

    experimental base class deriving from dictionary, rather than dynamic objects. cleaner get properties implementation; pushing just for future reference

---
+++ Core/Core/Models/DynamicBaseV2.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using Speckle.Core.Kits;
+
+namespace Speckle.Core.Models
+{
+  public class BaseV2 : Dictionary<string, object>
+  {
+    [IgnoreMember]
+    new public object this[string key]
+    {
+      get
+      {
+        if (ContainsKey(key)) return this[key];
+        return GetType().GetProperty(key)?.GetValue(this);
+      }
+      set
+      {
+        var prop = GetTypedProperty(GetType(), key);
+        if (prop != null)
+        {
+          prop.SetValue(this, value);
+        }
+        else
+        {
+          this[key] = value;
+        }
+      }
+    }
+
+    static Dictionary<Type, List<PropertyInfo>> propInfoCache = new Dictionary<Type, List<PropertyInfo>>();
+
+    private static void PopulatePropInfoCache(Type type)
+    {
+      if (!propInfoCache.ContainsKey(type))
+      {
+        propInfoCache[type] = type.GetProperties().Where(p => p.CanRead && p.CanWrite && p.DeclaringType != typeof(Dictionary<string, object>) && p.GetCustomAttribute(typeof(IgnoreMember)) == null).ToList();
+      }
+    }
+
+    private static PropertyInfo GetTypedProperty(Type type, string name)
+    {
+      PopulatePropInfoCache(type);
+      return propInfoCache[type].FirstOrDefault(prop => prop.Name == name);
+    }
+
+    /// <summary>
+    /// Gets all the accessible typed properties of this object.
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerable<PropertyInfo> GetInstanceProperties(bool ignoreAttributes = true)
+    {
+      var type = GetType();
+      PopulatePropInfoCache(type);
+      if (ignoreAttributes)
+      {
+        return propInfoCache[type];
+      }
+
+      return propInfoCache[type].Where(p => p.GetCustomAttribute(typeof(SchemaIgnore)) == null && p.GetCustomAttribute(typeof(ObsoleteAttribute)) == null);
+    }
+
+    /// <summary>
+    /// Gets all the dynamic property names (dictionary keys) of this object.
+    /// </summary>
+    /// <param name="ignoreAttributes">Whether to ignore properties starting with "__" (e.g., "__hiddenProp").</param>
+    /// <returns></returns>
+    private IEnumerable<string> GetDynamicPropertyNames(bool ignoreAttributes = true)
+    {
+      if (ignoreAttributes)
+      {
+        return Keys;
+      }
+      return Keys.Where(key => !key.StartsWith("__"));
+    }
+
+    public IEnumerable<PropertyDetails> GetProperties(PropertyType propertyType = PropertyType.All, bool ignoreAttributes = true)
+    {
+      if(propertyType == PropertyType.All || propertyType == PropertyType.Typed)
+      {
+        foreach (var prop in GetInstanceProperties(ignoreAttributes))
+          yield return new PropertyDetails { key = prop.Name, propertyInfo = prop };
+      }
+
+      if (propertyType == PropertyType.All || propertyType == PropertyType.Dynamic)
+      {
+        foreach(var key in GetDynamicPropertyNames())
+          yield return new PropertyDetails { key = key };
+      }
+    }
+
+    public Dictionary<string, object> CopyToDictionary()
+    {
+      var copy = new Dictionary<string, object>();
+      foreach (var (key, _) in GetProperties())
+      {
+        copy[key] = this[key];
+      }
+      return copy;
+    }
+
+    public new IEnumerator GetEnumerator()
+    {
+      foreach (var (key, _) in GetProperties())
+        yield return this[key];
+    }
+  }
+
+  internal class IgnoreMember : Attribute { }
+
+  public struct PropertyDetails
+  {
+    public string key;
+    public PropertyInfo propertyInfo;
+
+    public void Deconstruct( out string key, out PropertyInfo propertyInfo)
+    {
+      key = this.key;
+      propertyInfo = this.propertyInfo;
+    }
+  }
+
+  public enum PropertyType { Dynamic, Typed, All }
+
+}
+
---
+++ Core/Tests/BaseV2Tests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+using Speckle.Core.Models;
+using Tests;
+using System.Reflection;
+
+namespace Tests
+{
+  [TestFixture]
+  public class BaseV2Tests
+  {
+    [Test]
+    public void CanGetAndSetProperties()
+    {
+      var @base = new BaseV2() {
+        { "foo", "foo" },
+        { "bar", "bar" },
+        { "qux", "qux" },
+        { "Item", 42 },
+        { "💥", "💥" }
+      };
+
+      var props = @base.GetProperties().Select(prop => prop.key).ToList();
+      
+      Assert.AreEqual(props.Count, 5);
+      Assert.Contains("foo", props);
+      Assert.Contains("bar", props);
+      Assert.Contains("qux", props);
+      Assert.Contains("Item", props);
+      Assert.Contains("💥", props);
+    }
+
+    [Test]
+    public void SetsPropsInvariant()
+    {
+      var point = new FOM.Point3D { x = 10, y = 10, z = 10 };
+
+      point["x"] = 20;
+      point.y = 20;
+      Assert.AreEqual(point.x, 20);
+      Assert.AreEqual(point.y, 20);
+    }
+  }
+
+}
+
+namespace FOM
+{
+  public class Point3D : BaseV2
+  {
+    public double x { get; set; }
+    public double y { get; set; }
+    public double z { get; set; }
+  }
+
+  public class Point4D : Point3D
+  {
+    public double w { get; set; }
+  }
+
+  public class Line : BaseV2
+  {
+    public Point3D start { get; set; }
+    public Point3D end { get; set; }
+  }
+}
