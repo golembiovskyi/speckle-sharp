f00e1f9acb48171dacfb430d7987da877733f878
Claire Kuang <kuang.claire@gmail.com>
2022-08-04 09:02:37 +0100

    added avalonia window handler slns

---
+++ ConnectorAutocadCivil/ConnectorAutocadCivil/UI/Panel.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Autodesk.AutoCAD.Windows;
+using DesktopUI2.Views;
+
+namespace Speckle.ConnectorAutocadCivil
+{
+  /// <summary>
+  /// Interaction logic for Page1.xaml
+  /// </summary>
+  public partial class Panel
+  {
+    public static DockablePalette palette = new DockablePalette();
+    public static PaletteSet = null;
+
+    public Panel()
+    {
+      InitializeComponent();
+      AvaloniaHost.MessageHook += AvaloniaHost_MessageHook;
+    }
+
+    private const UInt32 DLGC_WANTARROWS = 0x0001;
+    private const UInt32 DLGC_HASSETSEL = 0x0008;
+    private const UInt32 DLGC_WANTCHARS = 0x0080;
+    private const UInt32 WM_GETDLGCODE = 0x0087;
+
+    /// <summary>
+    /// WPF was handling all the text input events and they where not being passed to the Avalonia control
+    /// This ensures they are passed, see: https://github.com/AvaloniaUI/Avalonia/issues/8198#issuecomment-1168634451
+    /// </summary>
+    private IntPtr AvaloniaHost_MessageHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+    {
+      if (msg != WM_GETDLGCODE) return IntPtr.Zero;
+      handled = true;
+      return new IntPtr(DLGC_WANTCHARS | DLGC_WANTARROWS | DLGC_HASSETSEL);
+    }
+
+    /// <summary>
+    /// Switching documents in Autocad causes the Panel content to "reset", so we need to re-initialize the avalonia host each time
+    /// </summary>
+    public void Init()
+    {
+      AvaloniaHost.Content = new MainUserControl();
+    }
+
+    public void SetupDockablePane(Autodesk.AutoCAD.UI.DockablePaneProviderData data)
+    {
+      //data.FrameworkElement = this as FrameworkElement;
+      //data.InitialState = new Autodesk.Revit.UI.DockablePaneState();
+      //data.InitialState.DockPosition = DockPosition.Tabbed;
+      //data.InitialState.TabBehind = Autodesk.Revit.UI.DockablePanes.BuiltInDockablePanes.ProjectBrowser;
+    }
+
+  }
+}
